791f4668cec8e259b494ade3676ab00c82069b36
Aunim HASSAN <aunim.hassan@gmail.com>
2021-11-15 11:07:20 +0100

    feat: utils.js

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+    fetch('https://raw.githubusercontent.com/aunimhsn/data-format/main/json/users.json')
+        .then((response) => {return response.json() })
+        .then((data) => {
+    
+    // TP 1. ----
+
+
+
+});
