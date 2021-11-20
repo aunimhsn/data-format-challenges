bc7e2e1f16b4cba9304397ea3baaffdd3614aedf
Aunim HASSAN <aunim.hassan@gmail.com>
2021-11-20 17:41:17 +0100

    feat: solutions

---
+++ js/challenges/utils.js
@@ -0,0 +1,53 @@
+// fetch('./json/users.json')
+
+
+fetch('https://raw.githubusercontent.com/aunimhsn/data-format/main/json/users.json')
+.then((response) => { return response.json() })
+.then((data) => { 
+
+    console.log(data);
+
+    // TP 1. ----
+
+    // 1
+
+    // 2
+
+    // 3
+
+    // 4
+
+    // 5
+
+    // 6
+
+    // 7
+
+    // 8
+
+    // 9
+
+    // 10
+
+    // 11
+
+
+    // TP 2. ----
+
+    // 1
+
+    // 2 
+
+    // 3 
+
+    // 4 
+
+    // 5
+
+    // 6
+
+    // 7 
+
+});
+
+
--- js/utils.js
+++
@@ -1,9 +0,0 @@
-    fetch('https://raw.githubusercontent.com/aunimhsn/data-format/main/json/users.json')
-        .then((response) => {return response.json() })
-        .then((data) => {
-    
-    // TP 1. ----
-
-
-
-});
